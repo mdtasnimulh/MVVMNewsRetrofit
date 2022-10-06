1e399b4d6192554c1191024b2a9fc1ec71b96bd1
mdtasnimulh <mdtasnimulh@gmail.com>
2022-10-06 10:19:04 +0600

    initial commit 2

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "3df322839bd44ffabb438d4632a096c1"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
